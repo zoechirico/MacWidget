e75f88e13b5d400e2c07a7c5d8c1af84312f27ad
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-16 17:00:10 -0500

    Added something to main menu..

--- MacWidget/ContentView.swift
+++ MacWidget/ContentView.swift
@@ -9,9 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     @Binding var document: MacWidgetDocument
-
+    @State var text:String = "Hello World"
     var body: some View {
-        TextEditor(text: $document.text)
+        
+        VStack{
+            Spacer()
+            Button(action: {
+                
+                text = "here 1"
+            }) {
+                Text("here 1")
+                
+            }
+            Button(action: {
+                text = "here 2"
+            }) {
+                Text("here 2")
+            }
+            
+            Text(text)
+                .padding(.all,10)
+                .padding([.top,.bottom],10)
+                .background(Color.orange)
+                .foregroundColor(Color.black)
+                .cornerRadius(5)
+                .shadow(radius: 25)
+                .font(Font.custom("Avenir-Black", size: 17))
+            Spacer()
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
